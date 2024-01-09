2881538032e1454758c0ab1173b8db6ac1940e1c
Jonathan Henrique <silva.henriquejonas@gmail.com>
2024-01-09 16:03:26 -0300

    Mudando padrao de datas a nivel de atributo

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -53,7 +53,8 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     private String imageUrl;
     @Column(nullable = false)
 //    retorna a data no formato esperado
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
     @Column(nullable = false)
     //    retorna a data no formato esperado
